86741bae7809c415898810a5545a216cc564dedc
KseniaUshakova <ksenia.ushakova@gmail.com>
2018-08-06 12:40:49 +1000

    Добавлено несколько функций

--- sandbox/src/main/java/ru/testing_education/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/testing_education/sandbox/MyFirstProgram.java
@@ -5,7 +5,33 @@ public class MyFirstProgram {
   public static void main(String[] args)
 
   {
-    System.out.println("Hello, World Test2!");
+    hello("Everybody");
+
+
+    double len=5;
+
+    System.out.println("Площадь кврадрата со стороной " + len +" = " + area (len));
+
+    double ab=5;
+    double bc=6;
+
+    System.out.println("Площадь прямоугольника со сторонами " + ab + " и " + bc + "=" + area(ab,bc));
+
   }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody);
+  }
+
+  public static double area (double l){
+    return l*l;
+
+  }
+
+  public static double area (double a, double b){
+    return a*b;
+
+  }
+
 
 }
