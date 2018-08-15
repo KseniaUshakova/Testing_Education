3e731da481678b91b3ab7e2ed8113d7cfa3fd90b
KseniaUshakova <ksenia.ushakova@gmail.com>
2018-08-15 20:05:34 +1000

    ContactHelper is modified

--- addressbook-web-tests/src/test/java/ru/testing_education/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/testing_education/addressbook/appmanager/ContactHelper.java
@@ -4,44 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.testing_education.addressbook.model.ContactInfo;
 
-public class ContactHelper {
-
-  private FirefoxDriver wd;
+public class ContactHelper extends HelperBase{
 
   public ContactHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void submitContactCreation() {
-    wd.findElement(By.name("theform")).click();
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    click(By.name("theform"));
+    click(By.xpath("//div[@id='content']/form/input[21]"));
   }
 
   public void fillContactDetails(ContactInfo contactInfo) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactInfo.getFirstName());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(contactInfo.getMiddleName());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactInfo.getSecondName());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactInfo.getAddress());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(contactInfo.getHomePhone());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactInfo.getEmail1());
-    wd.findElement(By.name("email2")).click();
-    wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys(contactInfo.getEmail2());
+    type(By.name("firstname"), contactInfo.getFirstName());
+    type(By.name("middlename"), contactInfo.getMiddleName());
+    type(By.name("lastname"), contactInfo.getSecondName());
+    type(By.name("address"), contactInfo.getAddress());
+    type(By.name("home"), contactInfo.getHomePhone());
+    type(By.name("email"),contactInfo.getEmail1());
+    type(By.name("email2"),contactInfo.getEmail2());
   }
 
   public void initContactCreation(String s) {
-    wd.findElement(By.linkText(s)).click();
+    click(By.linkText(s));
   }
 }
