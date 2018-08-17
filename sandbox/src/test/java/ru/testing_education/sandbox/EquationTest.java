cfc021b71f57f06445a8172a8b4038ff37f32cdf
KseniaUshakova <ksenia.ushakova@gmail.com>
2018-08-17 12:59:43 +1000

    Lesson3: adding of if constrauction for educational purpose (Equation)

---
+++ sandbox/src/test/java/ru/testing_education/sandbox/EquationTest.java
@@ -0,0 +1,30 @@
+package ru.testing_education.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class EquationTest {
+
+  @Test
+  public void test0() {
+    Equation e = new Equation(1, 1, 1);
+
+    Assert.assertEquals(e.rootNumber(),0);
+  }
+
+  @Test
+  public void test1() {
+    Equation e = new Equation(1, 2, 1);
+
+    Assert.assertEquals(e.rootNumber(),1);
+  }
+
+  @Test
+  public void test2() {
+    Equation e = new Equation(1, 6, 1);
+
+    Assert.assertEquals(e.rootNumber(),2);
+  }
+
+
+}
